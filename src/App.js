fe595a303145b638b4569463be8f5efac414c1b2
Valentin Paul <valentin@vamptech.de>
2022-06-24 11:41:00 +0200

    added increase and decrease functions

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import { useState } from 'react';
 
 function App() {
 
-const {number, setNumber} = useState();
+const [number, setNumber] = useState(0);
 
+const decrease = ()=>{
+  setNumber((prevNumber)=>{
+    return prevNumber -1;
+  })
+}
+
+const increase = ()=>{
+  setNumber((prevNumber)=>{
+    return prevNumber +1;
+  })
+}
 
   return (
     <div className="App">
       <header className="App-header">
       <h1>Counter</h1>
-      <h2>{number}</h2>
+      <h1>{number}</h1>
         <div className='buttons'>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={()=>{decrease()}}>-</button>
+        <button onClick={()=>{increase()}}>+</button>
       </div>
       <p>
         Click Buttons to increase/decrease the number!
